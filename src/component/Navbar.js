b2da24804d9a732fed2fb1789e1d6a83a2aa25b9
RifqiHanif <rifqihanif24@gmail.com>
2023-03-14 11:09:29 +0700

    adding other way of navigation

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,24 +1,49 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink, Redirect, useHistory } from "react-router-dom";
 
 export default function Navbar() {
+  let history = useHistory();
+
+  const [redirectpg, setRedirect] = useState("");
+  if (redirectpg) {
+    return <Redirect to={redirectpg} />;
+  }
   return (
     <div>
       <Link to="/">Home</Link>
-
       <Link to="/Cart">Cart</Link>
+
+      {/* <link to={
+        {
+          pathname: "/produk",
+          state: {namaSaya: "rifqi", umurSaya : "18"}
+        }
+      }>
+        --------testing-------
+      </link> */}
       {/* <Link to="/Produk">Produk</Link> */}
       <NavLink //to throw value
+        exact
         to={{
           pathname: "/produk",
           state: {
-            namaSayua: "Rifqi",
-            UmurSaya: "24"
+            NamaSaya: "Rifqi",
+            UmurSaya: "24",
           },
         }}
+        activeStyle={{ background: "red" }}
       >
         Produk
       </NavLink>
+
+      <button //use redirect
+        onClick={() => {
+          setRedirect("/cart");
+        }}
+      >
+        testing button
+      </button>
+      <button onClick={() => history.push("/cart")}>history function</button>
     </div>
   );
 }
--- src/page/Produk.js
+++ src/page/Produk.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../component/Navbar";
 import iberia from "../assets/Iberia.jpg"
+import { useLocation } from "react-router-dom";
 
 export default function Produk() {
+
+  const data = useLocation()
   return (
     <div>
       <Navbar />
       <img src={iberia} alt="iberia"/>
+      <h1>Nama: {data.state.NamaSaya}, Umur:{data.state.UmurSaya}</h1>
     </div>
   );
 }
